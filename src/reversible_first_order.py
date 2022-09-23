910bc2dc98f52dcf12e16369d2b3070d7446b991
Francisco Bustamante <23560423+chicolucio@users.noreply.github.com>
2022-09-23 14:28:10 -0300

    Black code formating

--- src/reversible_first_order.py
+++ src/reversible_first_order.py
@@ -1,6 +1,6 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class ReversibleFirstOrder:
@@ -10,8 +10,15 @@ class ReversibleFirstOrder:
     The software considers the equilibrium Reactant <=> Product also expressed A <=> B.
     """
 
-    def __init__(self, rate_cte_forward, rate_cte_backward, reactant_initial_concentration,
-                 product_initial_concentration, time=10, points=100):
+    def __init__(
+        self,
+        rate_cte_forward,
+        rate_cte_backward,
+        reactant_initial_concentration,
+        product_initial_concentration,
+        time=10,
+        points=100,
+    ):
         """
         Parameters
         ----------
@@ -44,9 +51,11 @@ class ReversibleFirstOrder:
         np.array
             numpy array with n points corresponding to [A].
         """
-        return -((self.kb * self.B0 - self.kf * self.A0) *
-                 np.exp(-self.t * (self.kf + self.kb)) -
-                 self.kb * (self.A0 + self.B0)) / (self.kf + self.kb)
+        return -(
+            (self.kb * self.B0 - self.kf * self.A0)
+            * np.exp(-self.t * (self.kf + self.kb))
+            - self.kb * (self.A0 + self.B0)
+        ) / (self.kf + self.kb)
 
     @property
     def product_time(self):
@@ -71,8 +80,14 @@ class ReversibleFirstOrder:
         return self.product_time / self.reactant_time
 
     @staticmethod
-    def _plot_params(ax=None, time_unit='s', ylabel='Concentration', concentration_unit='mol/l',
-                     title=True, title_text=''):
+    def _plot_params(
+        ax=None,
+        time_unit="s",
+        ylabel="Concentration",
+        concentration_unit="mol/l",
+        title=True,
+        title_text="",
+    ):
         """
         Plot customization.
         """
@@ -81,39 +96,51 @@ class ReversibleFirstOrder:
 
         # grid and ticks settings
         ax.minorticks_on()
-        ax.grid(b=True, which='major', linestyle='--',
-                linewidth=linewidth - 0.5)
-        ax.grid(b=True, which='minor', axis='both',
-                linestyle=':', linewidth=linewidth - 1)
-        ax.tick_params(which='both', labelsize=size + 2)
-        ax.tick_params(which='major', length=6, axis='both')
-        ax.tick_params(which='minor', length=3, axis='both')
+        ax.grid(b=True, which="major", linestyle="--", linewidth=linewidth - 0.5)
+        ax.grid(
+            b=True, which="minor", axis="both", linestyle=":", linewidth=linewidth - 1
+        )
+        ax.tick_params(which="both", labelsize=size + 2)
+        ax.tick_params(which="major", length=6, axis="both")
+        ax.tick_params(which="minor", length=3, axis="both")
 
         # labels and size
         ax.xaxis.label.set_size(size + 4)
         ax.yaxis.label.set_size(size + 4)
 
-        ax.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+        ax.ticklabel_format(style="sci", axis="y", scilimits=(0, 0))
         ax.yaxis.major.formatter._useMathText = True
         ax.figure.canvas.draw()  # Update the text
-        order_magnitude = ax.yaxis.get_offset_text().get_text().replace('\\times', '')
+        order_magnitude = ax.yaxis.get_offset_text().get_text().replace("\\times", "")
         ax.yaxis.offsetText.set_visible(False)
-        if order_magnitude == concentration_unit == '':
-            ax.set_ylabel(f'{ylabel}')
+        if order_magnitude == concentration_unit == "":
+            ax.set_ylabel(f"{ylabel}")
         else:
-            ax.set_ylabel(f'{ylabel} / {order_magnitude} {concentration_unit}')
-        ax.set_xlabel(f'Time / {time_unit}')
+            ax.set_ylabel(f"{ylabel} / {order_magnitude} {concentration_unit}")
+        ax.set_xlabel(f"Time / {time_unit}")
         if not title:
             pass
-        elif title_text == '':
-            ax.set_title(r'Concentration vs time for Reactant $\rightleftarrows$ Product', fontsize=18)
+        elif title_text == "":
+            ax.set_title(
+                r"Concentration vs time for Reactant $\rightleftarrows$ Product",
+                fontsize=18,
+            )
         else:
             ax.set_title(title_text, fontsize=18)
 
         return ax
 
-    def plot_reaction_time(self, size=(10, 8), ax=None, time_unit='s', concentration_unit='mol/l',
-                           title=True, title_text='', label_reactant='Reactant', label_product='Product'):
+    def plot_reaction_time(
+        self,
+        size=(10, 8),
+        ax=None,
+        time_unit="s",
+        concentration_unit="mol/l",
+        title=True,
+        title_text="",
+        label_reactant="Reactant",
+        label_product="Product",
+    ):
         """
         Plots concentration vs time
 
@@ -146,26 +173,50 @@ class ReversibleFirstOrder:
         ax.plot(self.t, self.reactant_time, label=label_reactant)
         ax.plot(self.t, self.product_time, label=label_product)
 
-        self._plot_params(ax, time_unit, 'Concentration', concentration_unit, title, title_text)
+        self._plot_params(
+            ax, time_unit, "Concentration", concentration_unit, title, title_text
+        )
 
         handles, labels = ax.get_legend_handles_labels()
-        patch_kf = mpatches.Patch(color='white', label=' '.join(['$k_f =$', f'{self.kf}']))
-        patch_kb = mpatches.Patch(color='white', label=' '.join(['$k_b =$', f'{self.kb}']))
-        patch_A0 = mpatches.Patch(color='white', label=' '.join(['$[A]_0 =$', f'{self.A0}']))
-        patch_BO = mpatches.Patch(color='white', label=' '.join(['$[B]_0 =$', f'{self.B0}']))
+        patch_kf = mpatches.Patch(
+            color="white", label=" ".join(["$k_f =$", f"{self.kf}"])
+        )
+        patch_kb = mpatches.Patch(
+            color="white", label=" ".join(["$k_b =$", f"{self.kb}"])
+        )
+        patch_A0 = mpatches.Patch(
+            color="white", label=" ".join(["$[A]_0 =$", f"{self.A0}"])
+        )
+        patch_BO = mpatches.Patch(
+            color="white", label=" ".join(["$[B]_0 =$", f"{self.B0}"])
+        )
 
         handles.extend([patch_A0, patch_BO, patch_kf, patch_kb])
 
         xbbox = 0.5
         ybbox = -0.15
-        ax.legend(handles=handles, loc='upper center', fontsize=14, shadow=True, fancybox=True,
-                  bbox_to_anchor=(xbbox, ybbox), ncol=len(handles), columnspacing=0.2)
+        ax.legend(
+            handles=handles,
+            loc="upper center",
+            fontsize=14,
+            shadow=True,
+            fancybox=True,
+            bbox_to_anchor=(xbbox, ybbox),
+            ncol=len(handles),
+            columnspacing=0.2,
+        )
 
         return ax
 
-    def plot_reaction_quotient_time(self, size=(10, 8), ax=None, time_unit='s', q='Q',
-                                    title=True,
-                                    title_text=r'Reaction Quotient vs Time for Reactant $\rightleftarrows$ Product'):
+    def plot_reaction_quotient_time(
+        self,
+        size=(10, 8),
+        ax=None,
+        time_unit="s",
+        q="Q",
+        title=True,
+        title_text=r"Reaction Quotient vs Time for Reactant $\rightleftarrows$ Product",
+    ):
         """
         Plots reaction quotient vs time
         Parameters
@@ -190,8 +241,8 @@ class ReversibleFirstOrder:
         if ax is None:
             fig, ax = plt.subplots(figsize=size, facecolor=(1.0, 1.0, 1.0))
 
-        ax.plot(self.t, self.reaction_quotient, color='red')
+        ax.plot(self.t, self.reaction_quotient, color="red")
 
-        self._plot_params(ax, time_unit, q, '', title, title_text)
+        self._plot_params(ax, time_unit, q, "", title, title_text)
 
         return ax
